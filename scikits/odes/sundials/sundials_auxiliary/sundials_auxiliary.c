aa8669f0f5f141bfc5a5d23bef0cf2aac0f73783
Pavol Kišon <pavol.kison@gmail.com>
2014-04-02 14:49:57 +0200

    remove "inline" declarations as it causes troubles when compiling with clang

--- scikits/odes/sundials/sundials_auxiliary/sundials_auxiliary.c
+++ scikits/odes/sundials/sundials_auxiliary/sundials_auxiliary.c
@@ -4,29 +4,29 @@
 #include <nvector/nvector_serial.h>
 
 /* N_Vector content access functions */
-inline N_VectorContent_Serial nv_content_s(N_Vector v) {
+N_VectorContent_Serial nv_content_s(N_Vector v) {
     return (N_VectorContent_Serial)(v->content);
 }
 
-inline long int nv_length_s(N_VectorContent_Serial vc_s) {
+long int nv_length_s(N_VectorContent_Serial vc_s) {
     return vc_s->length;
 }
 
-inline booleantype nv_own_data_s(N_VectorContent_Serial vc_s) {
+booleantype nv_own_data_s(N_VectorContent_Serial vc_s) {
     return vc_s->own_data;
 }
 
-inline realtype* nv_data_s(N_VectorContent_Serial vc_s) {
+realtype* nv_data_s(N_VectorContent_Serial vc_s) {
     return vc_s->data;
 }
 
 typedef realtype *nv_content_data_s;
 
-inline realtype get_nv_ith_s(nv_content_data_s vcd_s, int i) {
+realtype get_nv_ith_s(nv_content_data_s vcd_s, int i) {
     return vcd_s[i];
 }
 
-inline void set_nv_ith_s(nv_content_data_s vcd_s, int i,
+void set_nv_ith_s(nv_content_data_s vcd_s, int i,
                           realtype new_value) {
     vcd_s[i] = new_value;
 }
@@ -34,58 +34,57 @@ inline void set_nv_ith_s(nv_content_data_s vcd_s, int i,
 /* Dense matrix: acces functions */
 typedef realtype *DlsMat_col;
 
-inline int get_dense_N(DlsMat A) {
+int get_dense_N(DlsMat A) {
     return A->N;
 }
 
-inline int get_dense_M(DlsMat A) {
+int get_dense_M(DlsMat A) {
     return A->M;
 }
 
-inline int get_band_mu(DlsMat A) {
+int get_band_mu(DlsMat A) {
     return A->mu;
 }
 
-inline int get_band_ml(DlsMat A) {
+int get_band_ml(DlsMat A) {
     return A->ml;
 }
-inline realtype* get_dense_col(DlsMat A, int j) {
+realtype* get_dense_col(DlsMat A, int j) {
     return (A->cols)[j];
 }
-    
-inline void set_dense_col(DlsMat A, int j, realtype *data) {
+
+void set_dense_col(DlsMat A, int j, realtype *data) {
     (A->cols)[j] = data;
 }
-    
-inline realtype get_dense_element(DlsMat A, int i, int j) {
+
+realtype get_dense_element(DlsMat A, int i, int j) {
     return (A->cols)[j][i];
 }
-    
-inline void set_dense_element(DlsMat A, int i, int j, realtype aij) {
+
+void set_dense_element(DlsMat A, int i, int j, realtype aij) {
     (A->cols)[j][i] = aij;
 }
 
 /* Band matrix acces functions */
-inline DlsMat_col get_band_col(DlsMat A, int j) {
+DlsMat_col get_band_col(DlsMat A, int j) {
     return ((A->cols)[j] + (A->s_mu));
 }
-    
-inline void set_band_col(DlsMat A, int j, realtype *data) {
+
+void set_band_col(DlsMat A, int j, realtype *data) {
     ((A->cols)[j]) = data;
 }
 
-inline realtype get_band_col_elem(DlsMat_col col_j, int i, int j) {
+realtype get_band_col_elem(DlsMat_col col_j, int i, int j) {
     return col_j[(i)-(j)];
 }
-    
-inline void set_band_col_elem(DlsMat_col col_j, int i, int j, realtype aij) {
+
+void set_band_col_elem(DlsMat_col col_j, int i, int j, realtype aij) {
     col_j[(i)-(j)] = aij;
 }
-inline realtype get_band_element(DlsMat A, int i, int j) {
+realtype get_band_element(DlsMat A, int i, int j) {
     return ((A->cols)[j][(i)-(j)+(A->s_mu)]);
 }
-    
-inline void set_band_element(DlsMat A, int i, int j, realtype aij) {
+
+void set_band_element(DlsMat A, int i, int j, realtype aij) {
     (A->cols)[j][(i)-(j)+(A->s_mu)] = aij;
 }
-
